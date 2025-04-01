558a6b44c97e0556f40fbcd13dc0f1e7e4e66cc0
Gaurav Singh <www.gschauhan2003@gmail.com>
2025-04-01 11:30:04 +0530

    sec

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import { Card } from "./ui/Card";
-import { movieList } from "./data/movieList";
+import { movieList,bollyMovie } from "./data/movieList";
 
 export const Body = () => {
-  const [movieList2, setMovieList2] = useState(movieList);
+  const [movieList2, setMovieList2] = useState(movieList,bollyMovie);
+  
 
   const filterTopRated = () => {
     console.log("Filtering...");
@@ -21,23 +22,31 @@ export const Body = () => {
     setMovieList2(movieList);
   };
 
+  const hindiMovie = () => {
+    setMovieList2(bollyMovie);
+  }
+   
+
   return (
     <>
       <h1 className="heading">Top IMDB movies</h1>
       <div className="buttons">
-      <button className="btn" onClick={filterTopRated}>
-        Top rated
-      </button>
       <button className="btn" onClick={allMovies}>
         All
+      </button>
+      <button className="btn" onClick={filterTopRated}>
+        Top rated
       </button>
       <button className="btn" onClick={moreMovies}>
         rated 8 
       </button>
+      <button className="btn" onClick={hindiMovie}>
+        Indian Movies
+      </button>
       </div>
       <div className="Body">
         {movieList2.map((movie) => (
-          <div className="cards" key={movie.name} >
+          <div className="cards" key={movie.key} >
             <Card  movie={movie} />
           </div>
         ))}
